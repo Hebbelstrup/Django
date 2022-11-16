d53e597d90cd10978498c3a9c9c408c60ebc0acd
hebbelstrup <hebbelstrup@live.com>
2022-11-16 15:31:10 +0100

    Testing first commit

--- firstproject/firstproject/urls.py
+++ firstproject/firstproject/urls.py
@@ -17,6 +17,20 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path("admin/", admin.site.urls),
+    path('polls/', include('polls.urls'))
+
 ]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='polls/', permanent=True)),
+]
+
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
--- firstproject/polls/models.py
+++ firstproject/polls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_data = models.DataTimeField('data published')
+
+class Choice(models.Model):
+    question = models.FoerignKey(q)
